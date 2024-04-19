0d664fdd99cb3ff2f9c62bc7596c3be9540f23e5
Ashterix <ashterix69@gmail.com>
2024-04-19 09:29:33 +0300

    Add DateTime Normalizer

--- src/Transformer/Transformer.php
+++ src/Transformer/Transformer.php
@@ -3,10 +3,12 @@
 namespace Ufo\RpcObject\Transformer;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -30,7 +32,11 @@ class Transformer
             $encoders = [new JsonEncoder()];
             $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
             $metadataAwareNameConverter = new MetadataAwareNameConverter($classMetadataFactory);
-            $normalizers = [new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter)];
+            $propertyAccessor = new ReflectionExtractor();
+            $normalizers = [
+                new DateTimeNormalizer(),
+                new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter, propertyTypeExtractor: $propertyAccessor),
+            ];
             static::$instance = new static(new Serializer($normalizers, $encoders));
         }
         return static::$instance->getSerializer();
