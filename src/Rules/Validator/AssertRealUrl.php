4a7c500595897cba581d1aa666d79c4b591753bd
Ashterix <ashterix69@gmail.com>
2024-05-13 14:17:11 +0300

    fix typehint problem

--- src/Rules/Validator/AssertRealUrl.php
+++ src/Rules/Validator/AssertRealUrl.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraints\Url;
 #[\Attribute]
 class AssertRealUrl extends Url
 {
-    public string $message = 'This value is not a valid URL.';
+    public $message = 'This value is not a valid URL.';
 
     public function validatedBy(): string
     {
