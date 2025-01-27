7a60c0f1190a700378560a7780a61023b0259ec8
makx <makx.work@gmail.com>
2025-01-27 17:29:06 +0200

    fix Assert\Url add requireTld

--- src/Rules/CallbackRules.php
+++ src/Rules/CallbackRules.php
@@ -11,10 +11,10 @@ class CallbackRules
     {
         return [
             new Assert\NotBlank(),
-            new Assert\Url(),
-            new Assert\Url(message: 'Must have a protocol', relativeProtocol: true,),
-            new Assert\Url(message: 'Invalid protocol', protocols: ['http', 'https'], relativeProtocol: true),
-            new AssertRealUrl(message: 'Callback does not respond'),
+            new Assert\Url(requireTld: true),
+            new Assert\Url(message: 'Must have a protocol', relativeProtocol: true, requireTld: true),
+            new Assert\Url(message: 'Invalid protocol', protocols: ['http', 'https'], relativeProtocol: true, requireTld: true),
+            new AssertRealUrl(message: 'Callback does not respond', requireTld: true),
         ];
     }
 
